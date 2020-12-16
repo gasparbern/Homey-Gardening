ab5242570a4601c66fb55b7a0fa9b0f3b0d51956
Tee Rex <me@teerex.com>
2020-12-15 22:10:50 -0800

    On branch master

---
+++ platforms/browser/www/js/process.js
@@ -0,0 +1,351 @@
+var _root = document.getElementById('root');
+var dom =
+		<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Life Hacks </h1>
+<p>Welcome to Life Hacks App. 
+My App will show you different videos about different Life Hacks linked from Youtube. </p> </center>
+					
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//Cooking life hacks
+var _root = document.getElementById('a1');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Cooking Life Hacks</h1>
+						<p>4 Simple & Fun Cooking Life Hacks </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+// 2nd life hack link
+var _root = document.getElementById('a2');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> 20 Amazing Science Life Hacks</h1>
+						<p>This video is a compilation of 20 awesome science experiments. </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//3rd hack
+var _root = document.getElementById('a3');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Smartpphone Life Hacks </h1>
+						<p> These smartphone hacks will give you something unexpectedlly!! </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//4th hack
+var _root = document.getElementById('a4');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> 5 Phone Hacks </h1>
+						<p> Done doing everything on your phone? Try this stuffs. </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//5th hack
+var _root = document.getElementById('a5');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Smashed Phone screen Hack </h1>
+						<p> If you dont have enough money to buy a new screen for your phone, try this. </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//6th
+var _root = document.getElementById('a6');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Phone Hack (phone cover)</h1>
+						<p> Try anything inside your house to make your own DIY phone cover</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//7th
+var _root = document.getElementById('a7');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Tech Hacks </h1>
+						<p> If you're bored then try doing something more effective. Try this 
+						.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//8th
+var _root = document.getElementById('a8');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Life Hacks v.2 </h1>
+						<p> You better watch out</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//9th
+var _root = document.getElementById('a9');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Smartphone Life Hacks </h1>
+						<p> Bored with your phone? Try something new.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//10th
+var _root = document.getElementById('b');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Food Hacks </h1>
+						<p> If youre bored cook it! </p> </center>
+					</div>
+				</div>
+			</div>	
+//11th
+var _root = document.getElementById('b1');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Culinary Hacks </h1>
+						<p> Afraid of buying expensive kitchen tools? Try these.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//12th
+var _root = document.getElementById('b2');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Egg Hacks </h1>
+						<p> Tired of eating a simple sunny-side-up egg? Be creative.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//13th
+var _root = document.getElementById('b3');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Cooking Tips and Hacks </h1> 
+						<p> Listen carefully with these hacks, it might help you big.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//11th
+ReactDOM.render(dom,_root);
+//14th
+var _root = document.getElementById('b4');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Spendless Hacks </h1>
+						<p> 28 cool hacks that will cost you less peso </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//15th
+var _root = document.getElementById('b5');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Phone Hacks + DIYs </h1>
+						<p> 15 Easy Phone Hacks + DIYs You Should Know!</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//16th
+var _root = document.getElementById('b6');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Glue-Gun Hacks </h1>
+						<p> Stickeee? Stick thee. </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//17th
+var _root = document.getElementById('b7');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Funny Hacks </h1>
+						<p> These kind of hacks will blow your mind.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//18th
+var _root = document.getElementById('b8');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Simple Life Hacks </h1>
+						<p> Bored? Try me.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//19th
+var _root = document.getElementById('b9');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Phone Hacks </h1>
+						<p> Bored with your seemless phone? Craft it out.</p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+//20th
+var _root = document.getElementById('b10');
+	var dom =
+			<div class="page-content">
+				<div class="content-block">
+					<div class="content-block-inner">
+						<center>
+						<h1> Phone Hacks </h1>
+						<p>Every phone owner should watch this! </p> </center>
+					</div>
+				</div>
+			</div>	
+ReactDOM.render(dom,_root);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var setTimer = 0;
+// function clock(){
+// 	let timer = <h1>{new Date().toLocaleTimeString()}</h1>;
+// 	ReactDOM.render(timer,document.getElementById('timer'));	
+
+// 	setTimer = setTimeout(function(){
+// 		clock();
+// 	},1000);
+// };
+
+// function stopClock(){
+// 	clearInterval(setTimer);
+// }
+
+// let btn = <div>
+// 				<div id='timer' className='text'></div>
+// 				<div id='execute' className='text'></div>
+// 				<div id='output' className='text'></div>
+// 				<div id='error' className='text'></div>
+// 				<div id='commands' className='text'></div>
+// 				<div id='dbfile' className='text'></div>
+// 				<div id='savedb' className='text'></div>
+// 				<button onClick={clock}>Start Clock</button>
+// 				<button onClick={stopClock}>Stop Clock</button>
+// 			</div>;
+
+// ReactDOM.render(btn,document.getElementById('rangie'));
